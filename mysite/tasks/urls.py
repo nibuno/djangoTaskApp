5ec733960e9c06e5249e71632e9ee0a9a7fc76a0
Yumihiki <nibu10.majik@gmail.com>
2024-01-28 15:16:30 +0900

    typo修正

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -4,6 +4,6 @@ from .views import TaskCreateView, TaskListView, TaskDeleteView, TaskEditView
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
-    path('delete/<int:pk>', TaskDeleteView.as_view(), name='todo_delete'),
+    path('delete/<int:pk>', TaskDeleteView.as_view(), name='task_delete'),
     path('edit/<int:pk>', TaskEditView.as_view(), name='task_edit'),
 ]
