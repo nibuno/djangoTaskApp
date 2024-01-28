4ae0a879ae9632fff7cd3057a75468b8fed975b0
Yumihiki <nibu10.majik@gmail.com>
2024-01-28 15:30:20 +0900

    itemをtaskとtypoしていたので修正

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -8,7 +8,7 @@ from .models import Task
 class TaskListView(ListView):
     model = Task
     template_name = 'task/task_list.html'
-    context_object_name = 'items'
+    context_object_name = 'tasks'
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
