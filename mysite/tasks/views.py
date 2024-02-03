52160382c68801d0d0644a939e10ffebf44d9c16
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-02-04 08:38:30 +0900

    画面側でも期限を登録・表示できるように設定

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -15,7 +15,7 @@ class TaskListView(ListView):
 class TaskCreateView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
     model = Task
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'limit_date']
     template_name = 'task/task_create.html'
     success_url = reverse_lazy('task_list')
 
@@ -41,7 +41,7 @@ class TaskDeleteView(LoginRequiredMixin, DeleteView):
 class TaskEditView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     model = Task
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'limit_date']
     template_name = 'task/task_edit.html'
     success_url = reverse_lazy('task_list')
 
