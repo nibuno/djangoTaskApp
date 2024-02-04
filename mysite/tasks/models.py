f66065be0ab500c86f7f9be76e8a2df8b88506f9
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-02-04 19:14:54 +0900

    verbose_nameが抜けていたので追加

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     """タスクモデル"""
     title = models.CharField("タイトル", max_length=100)
     content = models.TextField("本文", blank=True)
-    status = models.IntegerField("ステータス", choices=STATUS_CHOICES, default=0)
+    status = models.IntegerField(verbose_name="ステータス", choices=STATUS_CHOICES, default=0)
     created_user = models.ForeignKey(
         to="auth.User",
         on_delete=models.PROTECT,
@@ -20,8 +20,8 @@ class Task(models.Model):
         on_delete=models.PROTECT,
         related_name="updated_user",
     )
-    created_at = models.DateTimeField("投稿日", auto_now_add=True)
-    updated_at = models.DateTimeField("更新日", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="投稿日", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="更新日", auto_now=True)
 
     class Meta:
         db_table = 'task'
