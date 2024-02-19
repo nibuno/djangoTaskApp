4e35d27abb9c2c1eb78d449f3debba450f582aa4
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-02-19 20:12:13 +0900

    black適用

--- mysite/tasks/forms.py
+++ mysite/tasks/forms.py
@@ -11,4 +11,6 @@ class TaskSearchForm(forms.Form):
         choices=[("", "---------")] + list(STATUS_CHOICES),
         required=False,
     )
-    limit_date = forms.DateField(label="期限", required=False, widget=forms.DateInput(attrs={"type": "date"}))
+    limit_date = forms.DateField(
+        label="期限", required=False, widget=forms.DateInput(attrs={"type": "date"})
+    )
