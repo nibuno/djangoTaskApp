1d951b5f4cb085c829908dcde2a60297bd242453
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-03-07 21:10:35 +0900

    アカウント作成後、そのアカウントでログインできるように実装

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -14,3 +15,20 @@ class CreateUserView(CreateView):
         if request.user.is_authenticated:
             return redirect("task_list")
         return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """ユーザー作成後、ログインする
+
+        NOTE: https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+              の内容を元に実装
+        """
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("task_list")
+        else:
+            return super().post(request, *args, **kwargs)
