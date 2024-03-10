504ff918dbc7679cf9b1f0507b0e83e33cd2213c
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-03-10 14:51:13 +0900

    ruff format 実行

--- mysite/tasks/migrations/0004_task_status.py
+++ mysite/tasks/migrations/0004_task_status.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("tasks", "0003_task_limit_date"),
     ]
--- mysite/tasks/migrations/0006_alter_task_status.py
+++ mysite/tasks/migrations/0006_alter_task_status.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("tasks", "0005_task_order"),
     ]
